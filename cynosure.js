2c906ca7e96c982cfea70a4c016481ac267426ce
Shruti Sundaresh <Shruti Sundaresh>
2012-01-03 21:23:12 -0800

    first commit

---
+++ cynosure.js
@@ -0,0 +1,17 @@
+$.fn.cynosure = function(options) {
+var defaults = {
+background:'white'
+};
+var opts = $.extend(defaults, options);
+return this.animate({
+width: "80%";
+marginLeft: "0.6in",
+fontSize: "3em",
+borderWidth: "10px"
+
+});
+};
+
+$.fn.cynosure.defaults = {
+  background: 'white'
+};
