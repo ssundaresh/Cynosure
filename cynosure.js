fa435e4fda32457506b5527f6a01b69309b3e554
Shruti <karthikn@Karthiks-MacBook-Air.local>
2012-01-17 21:25:28 -0800

    Added delay to simulate hoverintent

--- cynosure.js
+++ cynosure.js
@@ -3,14 +3,14 @@ var defaults = {
 background:'white'
 };					
 var opts = $.extend(defaults, options);
-return this.find('div').hover( function(){
+return this.find('div').delay(300).hover( function(){
 	$(this).animate({
-	marginLeft: "0.6in"
+	fontSize: "+=150%"
 	});},
 	function(){
 		var orig = $.data(this, 'css');
 		$(this).animate({
-		marginLeft: "-=0.6in"
+		fontSize: "-=150%"
 		});
 	}
 	);
